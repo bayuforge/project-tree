3164b9117068474d49ad37e3541a41a39b449017
bayuforge <bayuanataw@gmail.com>
2025-09-18 00:27:09 +0800

    Update main.py
    A minor update and fixes for more structuring and fixing false logic in the code.

--- python/main.py
+++ python/main.py
@@ -7,18 +7,21 @@ class createNode:
         self.right = None
         self.root = value
 
+
 #A function to print each tree when it's has been initialized
 def print_tree(node:createNode):
     if (node == None):
-        print("---<empty>---")
+        print("---<empty>---\n")
         return
-      
+    
     #print each node with postorder traversal
-    print(f"value: {node.root}")
-    print("pos: left \n")
-    print_tree(node.left)  #recursive for efficient performance
-    print("pos: right \n")
+    print(f"value: {node.root}\n")
+    print("pos: left")
+    print_tree(node.left) #recursive for efficient performance
+    print("pos: right")
     print_tree(node.right) #recursive for efficient performance
+    print("done. \n")
+
 
 #Initialize the node
 mytree_one = createNode(10)
@@ -26,7 +29,6 @@ mytree_two = createNode(11)
 mytree_three = createNode(12)
 mytree_four = createNode(13)
 mytree_five = createNode(14)
-mytree_five = createNode(15)
 
 #Initialize the root and child
 mytree_one.left = mytree_two
@@ -34,5 +36,5 @@ mytree_one.right = mytree_three
 mytree_three.left = mytree_four
 mytree_three.right = mytree_five
 
+print(f"pos: Root")
 print_tree(mytree_one)
-
